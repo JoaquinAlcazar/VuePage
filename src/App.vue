8f8ffe979a477c0e13b8b64e919949e77a5bc307
shuohaohuang <shuohao.huang.7e7@itb.cat>
2024-05-09 23:12:37 +0200

    componete para mostrar la info de los juegos hecho

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,51 @@
-
-
 <template>
-  p
+  <div class="panel">
+    <viewer v-for="(value, key) in ContentMap" :key="key" class="gamesContent" :title="key" :description="value"
+      :image="ImageMap[key]"></viewer>
+
+  </div>
 </template>
 
+
+<script>
+import viewer from "@/components/ShowGame.vue"
+export default {
+  components: {
+    viewer
+  },
+  data() {
+    return {
+      ContentMap:
+      {
+        "Dead Cells": "Dead Cells es un videojuego híbrido entre el género de exploración de mazmorras o roguelite, y metroidvania, creando su propio género, “roguevania”, desarrollado por el estudio Motion Twin.1​ El juego fue lanzado para PC, Mac, Linux, Playstation 4, Nintendo Switch y Xbox One,1​ siendo lanzado en la plataforma de Steam el 6 de agosto de 2018, bajo la categoría de juego de acción e indie."
+        ,
+        "Pokemon XY": "Pokémon X y Pokémon Y, conocidos en Japón como Pocket Monsters X&Y (ポケットモンスター X&Y Poketto Monsutā Ekkusu & Wai?), son dos videojuegos de RPG y aventura, desarrollados por Game Freak y distribuidos por Nintendo para la consola portátil Nintendo 3DS, que fueron lanzados el 12 de octubre de 2013 tanto en Japón como en América, Europa y Australia.2​ Pertenecen a la serie de videojuegos Pokémon, e inauguran la sexta generación de la misma. Fueron anunciados el 8 de enero de 2013 durante el Pokémon Direct por Satoru Iwata, presidente de Nintendo.3​"
+      },
+      ImageMap: {
+
+        "Dead Cells":
+          "/src/components/img/Dead Cells.jpg",
+        "Pokemon XY": "/src/components/img/Pokemon.jpg"
+      },
+      PageState: ["Home", "CRUD", "Api"]
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.panel{
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 }
+.gamesContent {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
--- src/components/ShowGame.vue
+++ src/components/ShowGame.vue
@@ -1,16 +1,63 @@
 <template>
-    <img :src=image alt="imagen de {{title}}">
-    <p>{{title}}</p>
-    <p>{{description}}</p>
+    <section class="container">
+        <img :src="image" :alt="'imagen de ' + title">
+        <article class="info">
+            <h2 class="title">{{ title }}</h2>
+            <p class="desc"> {{ description }}</p>
+
+        </article>
+    </section>
 </template>
 
 <script>
 export default {
-    name:"Show-Image",
+    name: "Show-Image",
     props: {
         image: null,
         title: null,
         description: null
     }
+
 }
 </script>
+
+<style scoped>
+* {
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+}
+
+.container {
+    width: 400px;;
+    margin-bottom: 20px;
+}
+.info{
+    width: 90%;
+    border: 1px solid black;
+    border-radius: 4%;
+    background-color: rgb(255, 249, 249);
+
+}
+
+img {
+    height: 150px;
+}
+
+
+p {
+    overflow-y: auto;
+    max-height: 150px;
+    margin-left: 5px;
+    margin-right: 5px;
+    border-radius: 1px;
+}
+
+::-webkit-scrollbar {
+    width: 0px;
+    background: transparent;
+    /* make scrollbar transparent */
+}
+</style>
