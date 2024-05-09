dbb35c0a1130876c8564b3d0cdf447895b13c1ae
shuohaohuang <shuohao.huang.7e7@itb.cat>
2024-05-09 18:27:36 +0200

    c

---
+++ src/components/ShowGame.vue
@@ -0,0 +1,16 @@
+<template>
+    <img :src=image alt="imagen de {{title}}">
+    <p>{{title}}</p>
+    <p>{{description}}</p>
+</template>
+
+<script>
+export default {
+    name:"Show-Image",
+    props: {
+        image: null,
+        title: null,
+        description: null
+    }
+}
+</script>
